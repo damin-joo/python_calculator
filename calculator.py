8745b50cd7314c52df5deecd07f7071f383db04f
Damin <damin.joo0725@gmail.com>
2024-06-28 06:46:53 -0400

    num input created

--- calculator.py
+++ calculator.py
@@ -10,10 +10,16 @@ root.resizable(False, False)       # resize (x, y)
 root.configure(background='black')
 
 #TEXT
-num = 0
-e = Entry(root, font = "Helvetica 20 bold", justify="right")
-e.grid(row=0, sticky=N+E+S+W, columnspan=4, padx=3, pady=3, ipady=8)
-e.insert(0, num)
+entry = Entry(root, font = "Helvetica 20 bold", justify="right")
+entry.grid(row=0, sticky=N+E+S+W, columnspan=4, padx=3, pady=3, ipady=8)
+entry.insert(0, "0")
+
+#FUNCTION
+def update(numpad):
+    print(entry.get())
+    if entry.get() == "0":
+        entry.delete(0, END)
+    entry.insert(END, numpad)
 
 #BUTTON
 button_name = [
@@ -34,16 +40,17 @@ button_text = [
 
 for i in range(0, 5):
     for j in range(0, len(button_name[i])):
-        button_name[i][j] = Button(root, text=button_text[i][j], width=5, height=2)
+        text = button_text[i][j]
+        name = button_name[i][j]
 
-        if(button_text[i][j] == "0"):
-            button_name[i][j].grid(row=i+1, column=j, columnspan=2, sticky=N+E+S+W, padx=3, pady=3)
-        elif(button_text[i][j] == "."):
-            button_name[i][j].grid(row=i+1, column=j+1, sticky=N+E+S+W, padx=3, pady=3)
-        elif(button_text[i][j] == "="):
-            button_name[i][j].grid(row=i+1, column=j+1, sticky=N+E+S+W, padx=3, pady=3)
+        name = Button(root, text=text, width=5, height=2, command=lambda t=text: update(t))
+
+        if(text == "0"):
+            name.grid(row=i+1, column=j, columnspan=2, sticky=N+E+S+W, padx=3, pady=3)
+        elif(text == "." or text == "="):
+            name.grid(row=i+1, column=j+1, sticky=N+E+S+W, padx=3, pady=3)
         else:
-            button_name[i][j].grid(row=i+1, column=j, sticky=N+E+S+W, padx=3, pady=3)
+            name.grid(row=i+1, column=j, sticky=N+E+S+W, padx=3, pady=3)
 
 
 root.mainloop()
