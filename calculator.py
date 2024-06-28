2fed65462d2e650abb3b52c405419316e3dd382e
Damin <damin.joo0725@gmail.com>
2024-06-28 07:24:14 -0400

    simple calc fin

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 
 from tkinter import *
 
@@ -16,14 +17,35 @@ entry.insert(0, "0")
 
 #FUNCTION
 def update(numpad):
-    print(entry.get())
     if entry.get() == "0":
         entry.delete(0, END)
     entry.insert(END, numpad)
 
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, END)
+        entry.insert(END, result)
+    except Exception as err:
+        msgbox.showerror("Error", err)
+
+def clear():
+    entry.delete(0, END)
+
+def percentage():
+    to_int = eval(entry.get())
+    entry.delete(0, END)
+    entry.insert(END, to_int/100)
+
+def negative():
+    if entry.get().startswith("-"):
+        entry.delete(0, 1)
+    else:
+        entry.insert(0, "-")
+
 #BUTTON
 button_name = [
-    ["btn_clr", "btn_neg", "", "btn_div"], 
+    ["btn_clr", "btn_neg", "btn_perc", "btn_div"], 
     ["btn_7", "btn_8", "btn_9", "btn_mul"], 
     ["btn_4", "btn_5", "btn_6", "btn_sub"], 
     ["btn_1", "btn_2", "btn_3", "btn_add"], 
@@ -43,14 +65,29 @@ for i in range(0, 5):
         text = button_text[i][j]
         name = button_name[i][j]
 
-        name = Button(root, text=text, width=5, height=2, command=lambda t=text: update(t))
+        if text == "=":
+            name = Button(root, text=text, width=5, height=2, command=calculate)
+        elif text == "AC":
+            name = Button(root, text=text, width=5, height=2, command=clear)
+        elif text == "+/-":
+            name = Button(root, text=text, width=5, height=2, command=negative)
+        elif text == "%":
+            name = Button(root, text=text, width=5, height=2, command=percentage)
+        else:
+            name = Button(root, text=text, width=5, height=2, command=lambda t=text: update(t))
 
-        if(text == "0"):
+        if text == "0":
             name.grid(row=i+1, column=j, columnspan=2, sticky=N+E+S+W, padx=3, pady=3)
         elif(text == "." or text == "="):
             name.grid(row=i+1, column=j+1, sticky=N+E+S+W, padx=3, pady=3)
         else:
             name.grid(row=i+1, column=j, sticky=N+E+S+W, padx=3, pady=3)
+
+#LISTBOX
+##change disable
+
+
+##scientific calculator
 
 
 root.mainloop()
